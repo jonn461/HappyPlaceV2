ce3158a7336ccd00515bfc465caa365b3060774c
Kevin Schänzle <kevin@schaenzle.ch>
2021-03-09 14:18:52 +0100

    1st Commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Happy Place V2</title>
+    </head>
+    <body>
+        <div id="title"><h1>Happy Place</h1></div>
+        <header>
+            <nav>
+                <li><a href="">LOGIN</a></li>
+                <li><a href="">SIGN UP</a></li>
+            </nav>
+        </header>
+        <main>
+            <
+        </main>
+    </body>
+</html>
