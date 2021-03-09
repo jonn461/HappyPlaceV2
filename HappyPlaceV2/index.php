c6d54d96693fb45462eb5ece129ac214eb08c14d
jonn461 <kevin@schaenzle.ch>
2021-03-09 14:39:53 +0100

    base structur

--- HappyPlaceV2/index.php
+++ HappyPlaceV2/index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/stylesheet.css">
         <title>Happy Place V2</title>
     </head>
     <body>
@@ -16,6 +17,8 @@
         </header>
         <main>
             <div class="center">
+                <div id="map">
+                </div>
             </div>
         </main>
     </body>
